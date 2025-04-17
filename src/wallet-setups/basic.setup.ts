45790e184cd4fddcfd0b7fa5465e6686d13cf96f
Andrew718PLTS <andrii.polovets@mentolabs.xyz>
2025-04-17 13:46:23 +0200

    Fix: Synpress building cache with package versions higher 4.0.3 (#85)

--- src/wallet-setups/basic.setup.ts
+++ src/wallet-setups/basic.setup.ts
@@ -1,20 +1,17 @@
 import { defineWalletSetup } from "@synthetixio/synpress";
 import { MetaMask } from "@synthetixio/synpress/playwright";
-
-import { envHelper } from "@helpers/env/env.helper";
 import { magicStrings } from "@constants/magic-strings.constants";
+import { envHelper } from "@helpers/env/env.helper";
 
-const walletPassword = envHelper.getWalletPassword();
+const password = envHelper.getWalletPassword();
+const seed = envHelper.getSeedPhrase();
 
-export default defineWalletSetup(
-  walletPassword,
-  async (context, walletPage) => {
-    const metamask = new MetaMask(context, walletPage, walletPassword);
-    await metamask.importWallet(envHelper.getSeedPhrase());
-    await metamask.addNetwork(
-      envHelper.isMainnet()
-        ? magicStrings.chain.mainnet
-        : magicStrings.chain.testnet,
-    );
-  },
-);
+export default defineWalletSetup(password, async (context, walletPage) => {
+  const metamask = new MetaMask(context, walletPage, password);
+  await metamask.importWallet(seed);
+  await metamask.addNetwork(
+    envHelper.isMainnet()
+      ? magicStrings.chain.mainnet
+      : magicStrings.chain.testnet,
+  );
+});
