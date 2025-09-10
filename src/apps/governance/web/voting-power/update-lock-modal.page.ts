ed7f3ebb257147ed2ef81a586a4e2e82d843a85e
Andrew718PLTS <andrii.polovets@mentolabs.xyz>
2025-09-10 19:09:08 +0200

    feat: add and update lock tests

---
+++ src/apps/governance/web/voting-power/update-lock-modal.page.ts
@@ -0,0 +1,19 @@
+import { ElementFinderHelper } from "@helpers/element-finder/element-finder.helper";
+import { Button, Input, Label } from "@shared/web/elements/index";
+import { BasePage } from "@shared/web/base/base.page";
+
+export class UpdateLockModalPage extends BasePage {
+  constructor(protected override ef: ElementFinderHelper) {
+    super(ef);
+  }
+
+  headerLabel = new Label(this.ef.text("Update Lock #"));
+
+  amountInput = new Input(this.ef.dataTestId("updateLockAmountInput"));
+
+  calcelButton = new Button(this.ef.text("Cancel"));
+  topUpLockButton = new Button(this.ef.text("Top up lock"));
+  approveMentoButton = new Button(this.ef.dataTestId("approveMentoButton"));
+
+  staticElements = [this.headerLabel];
+}
--- src/apps/governance/web/voting-power/voting-power.service.ts
+++ src/apps/governance/web/voting-power/voting-power.service.ts
@@ -5,62 +5,52 @@ import { waiterHelper } from "@helpers/waiter/waiter.helper";
 import { timeouts } from "@constants/index";
 import { expect } from "@fixtures/test.fixture";
 import { loggerHelper } from "@helpers/logger/logger.helper";
+import { UpdateLockModalPage } from "./update-lock-modal.page";
+import { Button } from "@shared/web/elements";
 
 const log = loggerHelper.get("VotingPowerService");
 
 @ClassLog
 export class VotingPowerService extends BaseService {
   public override page: VotingPowerPage = null;
+  public updateLockModalPage: UpdateLockModalPage = null;
 
   constructor(args: IVotingPowerServiceArgs) {
-    const { page } = args;
+    const { page, updateLockModalPage } = args;
     super(args);
     this.page = page;
+    this.updateLockModalPage = updateLockModalPage;
   }
 
-  async topUpLock(amount: string): Promise<void> {
+  async createLock({ lockAmount }: { lockAmount: string }): Promise<void> {
     await waiterHelper.waitForAnimation();
-    await this.enterAmount(amount);
-    await this.handleTopUp();
+    await this.enterAmount(lockAmount);
+    await this.handleAction("create");
   }
 
-  async handleTopUp() {
-    if (await this.waitForActionButton("approveMentoButton")) {
-      log.debug("Approving mento first to be able to top up lock");
-      await this.page.approveMentoButton.click();
-      await this.verifyConfirmationPopupIsOpened();
-      await this.metamask.rawModule.confirmTransaction();
-      await this.metamask.rawModule.approveTokenPermission();
-      await this.verifyConfirmationPopupIsClosed();
-    }
-    if (
-      await this.waitForActionButton("topUpLockButton", { throwError: true })
-    ) {
-      log.debug("Topping up lock directly");
-      await this.page.topUpLockButton.click();
-      await this.verifyConfirmationPopupIsOpened();
-      await this.metamask.confirmTransaction();
-      await this.verifyConfirmationPopupIsClosed();
-    }
-    expect
-      .soft(
-        await this.page.lockUpdatedSuccessfullyNotificationLabel.waitForDisplayed(
-          timeouts.s,
-          {
-            errorMessage: "Top-up success notification is not displayed!",
-            throwError: false,
-          },
-        ),
-      )
-      .toBeTruthy();
+  async topUpLock({
+    lockAmount,
+    lockIndex,
+  }: {
+    lockAmount: string;
+    lockIndex: number;
+  }): Promise<void> {
+    await this.openExistingLockByIndex(lockIndex);
+    await this.updateLockModalPage.amountInput.enterText(lockAmount);
+    await this.handleAction("top-up");
+  }
+
+  async openExistingLockByIndex(lockIndex: number): Promise<void> {
+    await this.page.getExistingLockByIndex(lockIndex).click();
+    await this.updateLockModalPage.verifyIsOpen();
   }
 
   async waitForActionButton(
-    buttonName: "approveMentoButton" | "topUpLockButton",
+    button: Button,
     { throwError = false }: { throwError?: boolean } = {},
   ): Promise<boolean> {
-    return this.page[buttonName].waitForDisplayed(timeouts.xs, {
-      errorMessage: `${buttonName} is not there!`,
+    return button.waitForDisplayed(timeouts.xs, {
+      errorMessage: `${button.name} is not there!`,
       throwError,
     });
   }
@@ -128,8 +118,9 @@ export class VotingPowerService extends BaseService {
 
   async getCurrentLockValues(): Promise<{ veMento: number; mento: number }> {
     const veMentoValueText =
-      await this.page.existingLock.veMentoLabel.getText();
-    const mentoValueText = await this.page.existingLock.mentoLabel.getText();
+      await this.page.locksSummary.totalVeMentoLabel.getText();
+    const mentoValueText =
+      await this.page.locksSummary.totalLockedMentoLabel.getText();
     return {
       veMento: Number(veMentoValueText.replace(/,/g, "")),
       mento: Number(mentoValueText.replace(/,/g, "")),
@@ -141,8 +132,47 @@ export class VotingPowerService extends BaseService {
       (await this.page.veMentoReceiveLabel.getText()).replace(/ veMENTO/g, ""),
     );
   }
+
+  private async handleAction(action: "top-up" | "create") {
+    const page = action === "top-up" ? this.updateLockModalPage : this.page;
+    const actionButton =
+      action === "top-up"
+        ? this.updateLockModalPage.topUpLockButton
+        : this.page.lockMentoButton;
+    const successNotificationLabel =
+      action === "top-up"
+        ? this.page.updateLockSuccessfullyNotificationLabel
+        : this.page.createLockSuccessfullyNotificationLabel;
+
+    if (await this.waitForActionButton(page.approveMentoButton)) {
+      log.debug(`Approving mento first to be able to ${action} lock`);
+      await page.approveMentoButton.click();
+      await this.verifyConfirmationPopupIsOpened();
+      await this.metamask.rawModule.confirmTransaction();
+      await this.metamask.rawModule.approveTokenPermission();
+      await this.verifyConfirmationPopupIsClosed();
+    }
+
+    if (await this.waitForActionButton(actionButton, { throwError: true })) {
+      log.debug(`${action} lock directly`);
+      await actionButton.click();
+      await this.verifyConfirmationPopupIsOpened();
+      await this.metamask.confirmTransaction();
+      await this.verifyConfirmationPopupIsClosed();
+    }
+
+    expect
+      .soft(
+        await successNotificationLabel.waitForDisplayed(timeouts.s, {
+          errorMessage: `${action} success notification is not displayed!`,
+          throwError: false,
+        }),
+      )
+      .toBeTruthy();
+  }
 }
 
 interface IVotingPowerServiceArgs extends IBaseServiceArgs {
   page: VotingPowerPage;
+  updateLockModalPage: UpdateLockModalPage;
 }
